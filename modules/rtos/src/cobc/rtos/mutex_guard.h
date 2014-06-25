e0257677257f90dbb9e6f37029a58e325dbcc0e6
Fabian Greif <Fabian.Greif@dlr.de>
2014-06-25 09:51:49 +0000

    Added missing include

--- modules/rtos/src/cobc/rtos/mutex_guard.h
+++ modules/rtos/src/cobc/rtos/mutex_guard.h
@@ -9,58 +9,62 @@
 #ifndef COBC_RTOS_MUTEX_GUARD_HPP
 #define COBC_RTOS_MUTEX_GUARD_HPP
 
+#include <cobc/rtos/mutex.h>
+
 namespace cobc
 {
-    namespace rtos
+namespace rtos
+{
+
+/**
+ * Implements a RAII-style locking.
+ *
+ * Locks the Mutex when created and unlocks it on destruction.
+ *
+ * Example:
+ *
+ *     Mutex mutex;
+ *     ...
+ *     {
+ *         MutexGuard lock(mutex);
+ *         // Mutex is locked on creation of lock
+ *
+ *         ... do something ...
+ *
+ *         // End of scope for lock => Mutex is unlocked.
+ *     }
+ *
+ * \author    Fabian Greif
+ * \ingroup    rtos
+ */
+class MutexGuard
+{
+public:
+    /**
+     * Acquire the mutex.
+     *
+     * \param m
+     *         The mutex to acquire.
+     */
+    inline MutexGuard(Mutex& m) :
+        mutex(m)
     {
-        /**
-         * Implements a RAII-style locking.
-         *
-         * Locks the Mutex when created and unlocks it on destruction.
-         *
-         * Example:
-         *
-         *     Mutex mutex;
-         *     ...
-         *     {
-         *         MutexGuard lock(mutex);
-         *         // Mutex is locked on creation of lock
-         *
-         *         ... do something ...
-         *
-         *         // End of scope for lock => Mutex is unlocked.
-         *     }
-         *
-         * \author    Fabian Greif
-         * \ingroup    rtos
-         */
-        class MutexGuard
-        {
-        public:
-            /**
-             * Acquire the mutex.
-             *
-             * \param m
-             *         The mutex to acquire.
-             */
-            inline MutexGuard(Mutex& m) :
-                mutex(m)
-            {
-                mutex.acquire();
-            }
-
-            /**
-             * Release the mutex.
-             */
-            inline ~MutexGuard()
-            {
-                mutex.release();
-            }
-
-        private:
-            Mutex& mutex;
-        };
+        mutex.acquire();
     }
+
+    /**
+     * Release the mutex.
+     */
+    inline ~MutexGuard()
+    {
+        mutex.release();
+    }
+
+private:
+    Mutex& mutex;
+};
+
+}
 }
 
 #endif // COBC_RTOS_MUTEX_GUARD_HPP
