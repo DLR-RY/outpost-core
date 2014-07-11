159483fb07ac9c9986cb582d9c51fc43f2089c86
Muhammad Bassam <Muhammad.Bassam@dlr.de>
2014-07-11 16:11:20 +0000

    Formating the code

--- modules/rtos/src/cobc/hal/rtems-leon3/uart.cpp
+++ modules/rtos/src/cobc/hal/rtems-leon3/uart.cpp
@@ -12,6 +12,9 @@
 #include <fcntl.h>
 #include "uart.h"
 
+static const char * uartPath[] = { "/dev/apbuart0", "/dev/apbuart1",
+    "/dev/apbuart2", "/dev/apbuart3", "/dev/apbuart4", "/dev/apbuart5" };
+
 #define DEBUG_EN
 #ifdef DEBUG_EN
 #define DEBUG	printf
@@ -20,66 +23,78 @@
 #endif
 
 cobc::leon3::UART::UART(uint8_t id, uint32_t baudrate, uint8_t blocking) :
-	devHandle(0),
-	devId(id),
-	baud(baudrate),
-	devState(CLOSE),
-	txCount(0),
-	rxCount(0),
-	blockingMode(blocking)
+        devHandle(0), devId(id), baud(baudrate), devState(CLOSE), txCount(0),
+        rxCount(0), blockingMode(blocking)
 {
 }
 
-cobc::leon3::UART::~UART(){
+cobc::leon3::UART::~UART()
+{
     this->close();
 }
 
 void
 cobc::leon3::UART::open()
 {
-	devHandle = ::open(cobc::leon3::uartPath[this->devId], O_RDWR);
-	if(devHandle < 0){
-		DEBUG("Device not opened, %s:errno-> %d\n", cobc::leon3::uartPath[this->devId], errno);
-		return;
-	}
+    devHandle = ::open(uartPath[this->devId], O_RDWR);
+    if (devHandle < 0)
+    {
+        DEBUG("Device not opened, %s:errno-> %d\n",
+                uartPath[this->devId], errno);
+        return;
+    }
 
     // Configure Device
-    if(::ioctl(devHandle, APBUART_SET_BAUDRATE, baud) < 0) return;
-    if(::ioctl(devHandle, APBUART_SET_ASCII_MODE, 0) < 0) return;
-    if(::ioctl(devHandle, APBUART_CLR_STATS, 0) < 0) return;
-    if(::ioctl(devHandle, APBUART_SET_RXFIFO_LEN, 64) < 0) return;
-    if(::ioctl(devHandle, APBUART_SET_TXFIFO_LEN, 64) < 0) return;
-    if(::ioctl(devHandle, APBUART_SET_BLOCKING, blockingMode) < 0) return;
+    if (::ioctl(devHandle, APBUART_SET_BAUDRATE, baud) < 0)
+        return;
+    if (::ioctl(devHandle, APBUART_SET_ASCII_MODE, 0) < 0)
+        return;
+    if (::ioctl(devHandle, APBUART_CLR_STATS, 0) < 0)
+        return;
+    if (::ioctl(devHandle, APBUART_SET_RXFIFO_LEN, 64) < 0)
+        return;
+    if (::ioctl(devHandle, APBUART_SET_TXFIFO_LEN, 64) < 0)
+        return;
+    if (::ioctl(devHandle, APBUART_SET_BLOCKING, blockingMode) < 0)
+        return;
 
     this->devState = OPEN;
     this->dev_enable();
-	DEBUG("%s opened, %d\n", cobc::leon3::uartPath[this->devId], devState);
+    DEBUG("%s opened, %d\n", uartPath[this->devId], devState);
 }
 
-void cobc::leon3::UART::close(){
-	int retval = 0;
-	this->dev_disable();
-	if(devState == DISABLED) {
-		retval = ::close(devHandle);
-		if(retval >= 0) {
-			devState = CLOSE;
-			DEBUG("%s closed, %d\n", cobc::leon3::uartPath[this->devId], devState);
-		}
-	}
+void
+cobc::leon3::UART::close()
+{
+    int retval = 0;
+    this->dev_disable();
+    if (devState == DISABLED)
+    {
+        retval = ::close(devHandle);
+        if (retval >= 0)
+        {
+            devState = CLOSE;
+            DEBUG("%s closed, %d\n", uartPath[this->devId],
+                    devState);
+        }
+    }
 
 }
 
 std::size_t
-cobc::leon3::UART::read(uint8_t* data, std::size_t length, time::Duration timeout){
-	(void) timeout;
+cobc::leon3::UART::read(uint8_t* data,
+                        std::size_t length,
+                        time::Duration timeout)
+{
+    (void) timeout;
     std::size_t retval = 0;
-    void *src = static_cast<void*> (data);
+    void *src = static_cast<void*>(data);
 
-    if(devState != ENABLED || src == NULL)
+    if (devState != ENABLED || src == NULL)
         return 0;
 
     // Reading the byte buffer up till supplied length
-    if((retval = ::read(devHandle, src, length)) <= 0)
+    if ((retval = ::read(devHandle, src, length)) <= 0)
         return retval;
 
     this->rxCount += retval;
@@ -87,48 +102,60 @@ cobc::leon3::UART::read(uint8_t* data, std::size_t length, time::Duration timeou
 }
 
 std::size_t
-cobc::leon3::UART::write(const uint8_t* data, std::size_t length, time::Duration timeout){
-	(void) timeout;
-	std::size_t ret = 0;
-	uint8_t *src = const_cast<uint8_t*> (data);
+cobc::leon3::UART::write(const uint8_t* data,
+                         std::size_t length,
+                         time::Duration timeout)
+{
+    (void) timeout;
+    std::size_t ret = 0;
+    uint8_t *src = const_cast<uint8_t*>(data);
 
-	if(devState != ENABLED || src == NULL)
-		return 0;
+    if (devState != ENABLED || src == NULL)
+        return 0;
 
-	/* Write block of data to the device handle */
-	if((ret += ::write(devHandle, src, length)) <= 0)
-		return ret;
+    /* Write block of data to the device handle */
+    if ((ret += ::write(devHandle, src, length)) <= 0)
+        return ret;
 
-	this->txCount += ret;
-	return ret;
+    this->txCount += ret;
+    return ret;
 }
 
 void
-cobc::leon3::UART::flush(){
+cobc::leon3::UART::flush()
+{
     fflush(NULL);		//FIXME: All output streams will be flushed
 }
 
 int
-cobc::leon3::UART::dev_enable(){
+cobc::leon3::UART::dev_enable()
+{
     int retval = 0;
-    if(devState == OPEN || devState == DISABLED) {
+    if (devState == OPEN || devState == DISABLED)
+    {
         retval = ::ioctl(devHandle, APBUART_START, 0);
-        if(retval >= 0) {
+        if (retval >= 0)
+        {
             devState = ENABLED;
-			DEBUG("%s enabled, %d\n", cobc::leon3::uartPath[this->devId], devState);
+            DEBUG("%s enabled, %d\n", uartPath[this->devId],
+                    devState);
         }
     }
     return retval;
 }
 
 int
-cobc::leon3::UART::dev_disable(){
+cobc::leon3::UART::dev_disable()
+{
     int retval = 0;
-    if(devState != DISABLED){
+    if (devState != DISABLED)
+    {
         retval = ::ioctl(devHandle, APBUART_STOP, 0);
-        if(retval >= 0) {
+        if (retval >= 0)
+        {
             devState = DISABLED;
-			DEBUG("%s disabled, %d\n", cobc::leon3::uartPath[this->devId], devState);
+            DEBUG("%s disabled, %d\n", uartPath[this->devId],
+                    devState);
         }
     }
     return retval;
--- modules/rtos/src/cobc/hal/rtems-leon3/uart.h
+++ modules/rtos/src/cobc/hal/rtems-leon3/uart.h
@@ -15,141 +15,138 @@
 
 #include "../serial.h"
 
-extern "C" {
+extern "C"
+{
 #include <apbuart.h>
 }
 
-
 namespace cobc
 {
-	namespace leon3
-	{
-		static const char * uartPath[] = {
-		"/dev/apbuart0", "/dev/apbuart1", "/dev/apbuart2", "/dev/apbuart3", "/dev/apbuart4", "/dev/apbuart5"
-		};
+    namespace leon3
+    {
 
-		/*! \class UART
-		 * \brief GR712RC UART driver interface
-		 * \see		cobc::hal::Serial
-		 * \author	Muhammad Bassam <muhammad.bassam@dlr.de>
-		 *
-		 * This uses the APBUART driver register by Driver manager to access
-		 * through the system level IO calls.
-		 */
-		class UART : public hal::Serial
-		{
-			friend class DevPrinter;
-		public:
-			/* Device states */
-			enum States{
-				OPEN = 0,
-				CLOSE,
-				ENABLED,
-				DISABLED,
-				READY,
-				BUSY
-			};
+        /**
+         * GR712RC UART driver interface
+         * This uses the APBUART driver register by Driver manager to access
+         * through the system level IO calls.
+         *
+         * \see		cobc::hal::Serial
+         * \author	Muhammad Bassam <muhammad.bassam@dlr.de>
+         */
+        class UART : public hal::Serial
+        {
+            friend class DevPrinter;
+        public:
+            /* Device states */
+            enum States
+            {
+                OPEN = 0, CLOSE, ENABLED, DISABLED, READY, BUSY
+            };
 
-			/**
-			 * Create the object to access the device specified by
-			 * path which can be found in the AMBA plug&play map.
-			 *
-			 * \param id
-			 * 		ID if the device corresponding to its path on AMBA PnP
-			 * \param baud
-			 * 		Baud rate of the device
-			 * \param blocking
-			 * 		Blocking option for the device (APBUART_BLK_RX, APBUART_BLK_TX, APBUART_BLK_FLUSH)
-			 */
-			UART(uint8_t id, uint32_t baud, uint8_t blocking);
+            /**
+             * Create the object to access the device specified by
+             * path which can be found in the AMBA plug&play map.
+             *
+             * \param id
+             * 		ID if the device corresponding to its path on AMBA PnP
+             * \param baud
+             * 		Baud rate of the device
+             * \param blocking
+             * 		Blocking option for the device (APBUART_BLK_RX, APBUART_BLK_TX, APBUART_BLK_FLUSH)
+             */
+            UART(uint8_t id, uint32_t baud, uint8_t blocking);
 
             /**
              * \brief Virtual UART interface destructor
              *
              * Destroys the object and free resources.
              */
-			virtual
+            virtual
             ~UART();
 
-			/**
-			 * Opens the UART device
-			 */
-            virtual
-            void open();
+            /**
+             * Opens the UART device
+             */
+            virtual void
+            open();
 
             /**
-			 * Opens the UART device
-			 */
-            virtual
-            void close();
+             * Opens the UART device
+             */
+            virtual void
+            close();
 
             /**
-             * \brief Virtual method to check the device availability
-             *
-             * Private status field is updated during every device
-             * operation stage. The availability can be expressed as a
-             * READY state of the device
+             * Check the device availability. Status field is updated
+             * during every device operation stage. The availability
+             * can be expressed as a READY state of the device
              *
              * \return True if available, false if not available
              */
-            virtual
-            bool isAvailable(){
-				return ((devState == OPEN || devState == ENABLED) ? true : false);
-			}
+            virtual bool
+            isAvailable()
+            {
+                return ((devState == OPEN || devState == ENABLED) ? true : false);
+            }
 
-			/*!
-			 * Read is only successful if device is opened and in READY
-			 * state. Otherwise the request will discarded. THe data bytes
-			 * are being read in according to the length passed.
-			 *
-			 * \param *data
-			 * 		Pointer to a buffer big enough to storage `length` bytes
-			 * \param length
-			 * 		Number of bytes to be read
-			 * \param timeout
-			 * 		Timeout if the read is blocked and no data has been read within this time
-			 *
-			 * \return	Number of bytes which could be read, maximal `length`
-			 */
-			virtual std::size_t
-			read(uint8_t* data, std::size_t length, time::Duration timeout = time::Duration::infinity());
+            /**
+             * Read is only successful if device is opened and in READY
+             * state. Otherwise the request will discarded. THe data bytes
+             * are being read in according to the length passed.
+             *
+             * \param *data
+             * 		Pointer to a buffer big enough to storage `length` bytes
+             * \param length
+             * 		Number of bytes to be read
+             * \param timeout
+             * 		Timeout if the read is blocked and no data has been read within this time
+             *
+             * \return	Number of bytes which could be read, maximal `length`
+             */
+            virtual std::size_t
+            read(uint8_t* data, std::size_t length, time::Duration timeout =
+                         time::Duration::infinity());
 
-			/**
-			 * Write a block of bytes within a give duration
-			 *
-			 * \param *data
-			 * 		Pointer to a buffer big enough to storage `length` bytes
-			 * \param length
-			 * 		Number of bytes to be written
-			 * \param timeout
-			 * 		Timeout if the write is blocked and not enough data bytes has been written within this time
-			 *
-			 * \return	Number of bytes which could be read, maximal `length`
-			 */
-			virtual std::size_t
-			write(const uint8_t* data, std::size_t length, time::Duration timeout = time::Duration::infinity());
+            /**
+             * Write a block of bytes within a give duration
+             *
+             * \param *data
+             * 		Pointer to a buffer big enough to storage `length` bytes
+             * \param length
+             * 		Number of bytes to be written
+             * \param timeout
+             * 		Timeout if the write is blocked and not enough data bytes has been written within this time
+             *
+             * \return	Number of bytes which could be read, maximal `length`
+             */
+            virtual std::size_t
+            write(const uint8_t* data,
+                  std::size_t length,
+                  time::Duration timeout = time::Duration::infinity());
 
-			/**
-			 * Sends eventually buffered data. Afterwards all internal buffers
-			 * are empty.
-			 * TODO: Complete the implementation
-			 */
-			virtual
-			void flush();
+            /**
+             * Sends eventually buffered data. Afterwards all internal buffers
+             * are empty.
+             * TODO: Complete the implementation
+             */
+            virtual void
+            flush();
 
         private:
             // Private methods intended to be accessed only within object
-            int dev_enable();
-            int dev_disable();
+            int
+            dev_enable();
+            int
+            dev_disable();
 
-            int devHandle;              /** Handle to the device */
-			uint8_t devId;				/** Device name */
-			uint32_t baud;              /** Connected device baud rate */
-			enum States devState;	  	/** If the device is enabled */
-			unsigned int txCount;      	/** Total transmitted data count of interface */
-			unsigned int rxCount;      	/** Total received data count of interface */
-			uint8_t blockingMode;		/** Device blocking mode */
-			apbuart_stats devStatus;
+            int devHandle; /** Handle to the device */
+            uint8_t devId; /** Device name */
+            uint32_t baud; /** Connected device baud rate */
+            enum States devState; /** If the device is enabled */
+            unsigned int txCount; /** Total transmitted data count of interface */
+            unsigned int rxCount; /** Total received data count of interface */
+            uint8_t blockingMode; /** Device blocking mode */
+            apbuart_stats devStatus;
         };
     }
 }
