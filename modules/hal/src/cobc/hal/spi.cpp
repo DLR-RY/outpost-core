12e0b66124552f15ce775069e1e766bd28dae3f0
Fabian Greif <Fabian.Greif@dlr.de>
2014-10-10 12:40:41 +0000

    Update license information

--- modules/hal/src/cobc/hal/spi.cpp
+++ modules/hal/src/cobc/hal/spi.cpp
@@ -1,8 +1,16 @@
 /*
  * Copyright (c) 2014, German Aerospace Center (DLR)
- * All Rights Reserved.
  *
- * See the file "LICENSE" for the full license governing this code.
+ * This file is part of libCOBC 0.3-pre.
+ *
+ * It is distributed under the terms of the GNU General Public License with
+ * a linking exception. See the file "LICENSE" for the full license governing
+ * this code.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 // ----------------------------------------------------------------------------
 #include "spi.h"
--- tools/l3test/src/lua/module/bitstream/bitstream.cpp
+++ tools/l3test/src/lua/module/bitstream/bitstream.cpp
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2014, German Aerospace Center (DLR)
  *
- * This file is part of the libCOBC library.
+ * This file is part of libCOBC 0.3-pre.
  *
  * It is distributed under the terms of the GNU General Public License with
  * a linking exception. See the file "LICENSE" for the full license governing
