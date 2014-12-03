7d2d7ba81a25ba04c4bcfa7613a82d15c94b15ac
Fabian Greif <Fabian.Greif@dlr.de>
2014-12-03 13:34:10 +0000

    Update license header

--- tools/l3test/src/lua/module/crc16/crc16.h
+++ tools/l3test/src/lua/module/crc16/crc16.h
@@ -1,17 +1,18 @@
 /*
  * Copyright (c) 2014, German Aerospace Center (DLR)
- *
+ * 
  * This file is part of libCOBC 0.3-pre.
  *
- * It is distributed under the terms of the GNU General Public License with
- * a linking exception. See the file "LICENSE" for the full license governing
+ * It is distributed under the terms of the GNU General Public License with a
+ * linking exception. See the file "LICENSE" for the full license governing
  * this code.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  */
+// ----------------------------------------------------------------------------
 
 #ifdef __cplusplus
 extern "C"
