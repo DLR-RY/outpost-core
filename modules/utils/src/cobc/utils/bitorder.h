59c8dfb72e9a6d542ec364342afd63aadb75d623
Fabian Greif <Fabian.Greif@dlr.de>
2014-12-09 15:32:11 +0000

    Remove because static_assert (even with the helper file) does not compile without warnings outside a function body for C++03

--- modules/utils/src/cobc/utils/bitorder.h
+++ modules/utils/src/cobc/utils/bitorder.h
@@ -38,7 +38,8 @@ template <typename T, int startIn, int endIn>
 class BitorderMsb0ToLsb0
 {
 protected:
-    static_assert(startIn < endIn, "Invalid bitfield definition! 'startIn' must be smaller than 'endIn'");
+    // static assert only possible in C++11 :(
+    //static_assert(startIn < endIn, "Invalid bitfield definition! 'startIn' must be smaller than 'endIn'");
 
     static const int offsetByte = startIn & 7;
 
