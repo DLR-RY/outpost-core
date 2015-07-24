c0c5b912cc4615ffbf824b3ea7e2883dd0a4e5ac
Fabian Greif <Fabian.Greif@dlr.de>
2015-07-24 16:18:49 +0000

    Added topic logger to log data published to a topic

---
+++ modules/smpc/test/unittest/smpc/topic_logger.h
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2015, German Aerospace Center (DLR)
+ * 
+ * This file is part of libCOBC 0.4.
+ *
+ * It is distributed under the terms of the GNU General Public License with a
+ * linking exception. See the file "LICENSE" for the full license governing
+ * this code.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef UNITTEST_SMPC_TOPIC_LOGGER_H
+#define UNITTEST_SMPC_TOPIC_LOGGER_H
+
+#include <cobc/smpc/subscription.h>
+#include <cobc/smpc/topic.h>
+
+#include <unittest/smpc/testing_subscription.h>
+
+#include <list>
+
+namespace unittest
+{
+namespace smpc
+{
+
+template <typename T>
+class TopicLogger
+{
+public:
+    TopicLogger(cobc::smpc::Topic<T>& topic);
+
+    const T&
+    getNext() const;
+
+    void
+    dropNext();
+
+    bool
+    isEmpty() const;
+
+private:
+    typedef typename cobc::remove_const<T>::type NonConstType;
+
+    void
+    onReceive(T* item);
+
+    cobc::smpc::Subscription mSubscription;
+    std::list<NonConstType> mItems;
+};
+
+}
+}
+
+#include "topic_logger_impl.h"
+
+#endif
---
+++ modules/smpc/test/unittest/smpc/topic_logger_impl.h
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2015, German Aerospace Center (DLR)
+ * 
+ * This file is part of libCOBC 0.4.
+ *
+ * It is distributed under the terms of the GNU General Public License with a
+ * linking exception. See the file "LICENSE" for the full license governing
+ * this code.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef COBC_SMPC_TOPIC_LOGGER_IMPL_H
+#define COBC_SMPC_TOPIC_LOGGER_IMPL_H
+
+#include "topic_logger.h"
+
+template<typename T>
+unittest::smpc::TopicLogger<T>::TopicLogger(cobc::smpc::Topic<T>& topic) :
+    mSubscription(topic, this, &TopicLogger::onReceive)
+{
+}
+
+template<typename T>
+const T&
+unittest::smpc::TopicLogger<T>::getNext() const
+{
+    return mItems.front();
+}
+
+template<typename T>
+void
+unittest::smpc::TopicLogger<T>::dropNext()
+{
+    mItems.pop_front();
+}
+
+template<typename T>
+bool
+unittest::smpc::TopicLogger<T>::isEmpty() const
+{
+    return mItems.empty();
+}
+
+template<typename T>
+void
+unittest::smpc::TopicLogger<T>::onReceive(T* item)
+{
+    mItems.push_back(*item);
+}
+
+#endif
