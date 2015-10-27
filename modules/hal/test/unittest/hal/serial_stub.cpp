e66cfda4a9591b61a37ff2f2e4817df4273eed7e
Fabian Greif <Fabian.Greif@dlr.de>
2015-10-27 15:22:07 +0000

    [hal] SerialStub added. To be used to directly manipulate the serial send- and receive queues in unittests.

---
+++ modules/hal/test/unittest/hal/serial_stub.cpp
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2015, German Aerospace Center (DLR)
+ *
+ * This file is part of libCOBC 0.4.
+ *
+ * It is distributed under the terms of the GNU General Public License with a
+ * linking exception. See the file "LICENSE" for the full license governing
+ * this code.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+// ----------------------------------------------------------------------------
+
+#include "serial_stub.h"
+
+#include <string.h>
+
+using unittest::hal::SerialStub;
+
+SerialStub::SerialStub()
+{
+}
+
+SerialStub::~SerialStub()
+{
+}
+
+void
+SerialStub::close()
+{
+    mDataToReceive.clear();
+    mDataToTransmit.clear();
+}
+
+bool
+SerialStub::isAvailable()
+{
+    return !mDataToReceive.empty();
+}
+
+size_t
+SerialStub::getNumberOfBytesAvailable()
+{
+    return mDataToReceive.size();
+}
+
+size_t
+SerialStub::read(cobc::BoundedArray<uint8_t> data,
+                 cobc::time::Duration /*timeout*/)
+{
+    size_t length = data.getNumberOfElements();
+    size_t available = mDataToReceive.size();
+
+    if (length > available)
+    {
+        length = available;
+    }
+
+    if (length > 0)
+    {
+        memcpy(&data[0], &mDataToReceive[0], length);
+        mDataToReceive.erase(mDataToReceive.begin(), mDataToReceive.begin() + length);
+    }
+    return length;
+}
+
+size_t
+SerialStub::write(cobc::BoundedArray<const uint8_t> data,
+                  cobc::time::Duration /*timeout*/)
+{
+    mDataToTransmit.insert(mDataToTransmit.end(), data.begin(), data.end());
+    return data.getNumberOfElements();
+}
+
+void
+SerialStub::flush()
+{
+    // do nothing
+}
---
+++ modules/hal/test/unittest/hal/serial_stub.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2015, German Aerospace Center (DLR)
+ *
+ * This file is part of libCOBC 0.4.
+ *
+ * It is distributed under the terms of the GNU General Public License with a
+ * linking exception. See the file "LICENSE" for the full license governing
+ * this code.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef UNITTEST_HAL_SERIAL_STUB_H
+#define UNITTEST_HAL_SERIAL_STUB_H
+
+#include <vector>
+
+#include <cobc/hal/serial.h>
+
+namespace unittest
+{
+namespace hal
+{
+
+/**
+ * Serial interface stub.
+ *
+ * Provides direct access to the send- and receive queue.
+ *
+ * \author  Fabian Greif
+ */
+class SerialStub : public cobc::hal::Serial
+{
+public:
+    SerialStub();
+
+    virtual
+    ~SerialStub();
+
+    virtual void
+    close() override;
+
+    virtual bool
+    isAvailable() override;
+
+    virtual size_t
+    getNumberOfBytesAvailable() override;
+
+    virtual size_t
+    read(cobc::BoundedArray<uint8_t> data,
+         cobc::time::Duration timeout = cobc::time::Duration::maximum()) override;
+
+    virtual size_t
+    write(cobc::BoundedArray<const uint8_t> data,
+          cobc::time::Duration timeout = cobc::time::Duration::maximum()) override;
+
+    virtual void
+    flush() override;
+
+    /// Data which should be accessible through the read function
+    std::vector<uint8_t> mDataToReceive;
+
+    /// Data which has be stored through the write function
+    std::vector<uint8_t> mDataToTransmit;
+};
+
+}
+}
+
+#endif
