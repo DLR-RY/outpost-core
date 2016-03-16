844c8250fbea003bd4277eb20e3d47f4e9e41ee3
Fabian Greif <Fabian.Greif@dlr.de>
2016-03-16 17:01:49 +0000

    Update version to 0.5 and update Changelog accordingly

--- modules/hal/src/cobc/hal/register_impl.h
+++ modules/hal/src/cobc/hal/register_impl.h
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2014, German Aerospace Center (DLR)
  *
- * This file is part of the project 'ccsds_rt', version 0.1.
+ * This file is part of libCOBC 0.5.
  *
- * It is distributed under the terms of the GNU General Public License with
- * a linking exception. See the file "LICENSE" for the full license governing
+ * It is distributed under the terms of the GNU General Public License with a
+ * linking exception. See the file "LICENSE" for the full license governing
  * this code.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
  */
 // ----------------------------------------------------------------------------
 
--- modules/hal/test/unittest/hal/spacewire_stub.cpp
+++ modules/hal/test/unittest/hal/spacewire_stub.cpp
@@ -1,16 +1,14 @@
 /*
  * Copyright (c) 2015, German Aerospace Center (DLR)
  *
- * This file is part of libCOBC 0.4.
+ * This file is part of libCOBC 0.5.
  *
  * It is distributed under the terms of the GNU General Public License with a
  * linking exception. See the file "LICENSE" for the full license governing
  * this code.
  *
  * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY
-{
-} without even the implied warranty of MERCHANTABILITY or
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
  * more details.
  */
