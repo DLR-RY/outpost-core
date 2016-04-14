895c0614bb288fe948eeece9dd6bc4dec63e7ac7
Fabian Greif <Fabian.Greif@dlr.de>
2016-04-14 15:15:52 +0000

    Tool to modify the gtest output for Jenkins

---
+++ tools/gtest_process_skipped.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# 
+# Jenkins xUnit processor requires that a skipped test contains
+# the <skipped/> element and not just setting status to "notrun".
+# This script adds the skipped tag for those tests.
+
+import os
+import sys
+import re
+
+notrun_filter = re.compile('(\s*<testcase[^<]+status="notrun"[^<]+)/>')
+
+with open(sys.argv[2], 'w') as output:
+	with open(sys.argv[1], 'r') as input:
+		for line in input:
+			
+			match = notrun_filter.match(line)
+			if match:
+				testcase = match.group(1)
+				output.write(testcase + "><skipped /></testcase>\n")
+			else:
+				output.write(line)
