e309e293662dbfa7bb81117c45d5add65c0a4aad
Fabian Greif <Fabian.Greif@dlr.de>
2016-11-03 12:46:28 +0000

    Update for compatibility with RTEMS4.12

--- modules/rtos/arch/rtems/cobc/rtos/clock.cpp
+++ modules/rtos/arch/rtems/cobc/rtos/clock.cpp
@@ -21,10 +21,8 @@
 cobc::time::SpacecraftElapsedTime
 cobc::rtos::SystemClock::now() const
 {
-    rtems_interval ticks_since_boot;
     rtems_interval us_per_tick = rtems_configuration_get_microseconds_per_tick();
-
-    rtems_clock_get(RTEMS_CLOCK_GET_TICKS_SINCE_BOOT, &ticks_since_boot);
+    rtems_interval ticks_since_boot = rtems_clock_get_ticks_since_boot();
 
     // convert to microseconds
     uint64_t us = static_cast<uint64_t>(ticks_since_boot) *
