5c4705e0acf1b92be3e6181018edbd3b358a7d5f
Fabian Greif <Fabian.Greif@dlr.de>
2017-03-16 16:58:48 +0100

    [utils] New interface for serializable objects.

---
+++ modules/utils/src/outpost/utils/serializable_object.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2017, German Aerospace Center (DLR)
+ *
+ * This file is part of the development version of OUTPOST.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Authors:
+ * - 2017, Fabian Greif (DLR RY-AVS)
+ */
+// ----------------------------------------------------------------------------
+
+#ifndef OUTPOST_UTILS_SERIALIZABLE_OBJECT_H
+#define OUTPOST_UTILS_SERIALIZABLE_OBJECT_H
+
+#include <stddef.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "serialize.h"
+
+namespace outpost
+{
+
+/**
+ * Abstract base class for objects than can be serialized.
+ *
+ * \author  Fabian Greif
+ */
+class SerializableObject
+{
+public:
+    virtual
+    ~SerializableObject();
+
+    /**
+     * Get the size of the parameter.
+     *
+     * \return  Size of the parameter in bytes.
+     */
+    virtual size_t
+    getSerializedSize() const = 0;
+
+    /**
+     * Write parameter value onto the output stream.
+     *
+     * \param stream
+     *      Output stream
+     */
+    virtual void
+    serialize(Serialize& stream) const = 0;
+
+    /**
+     * Read parameter value from stream.
+     *
+     * \param stream
+     *      Input stream
+     * \retval  true    Parameter could be read and validated.
+     * \retval  false   Parameter validation failed.
+     */
+    virtual bool
+    deserialize(Deserialize& stream) = 0;
+};
+
+
+}
+
+#endif
