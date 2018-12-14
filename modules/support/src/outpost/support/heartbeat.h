3d75e054ab4c94b187ceecd3a74dd3ebf75bc806
Fabian Greif <Fabian.Greif@dlr.de>
2018-12-14 17:59:09 +0100

    Fix license header

--- modules/support/src/outpost/support/heartbeat.h
+++ modules/support/src/outpost/support/heartbeat.h
@@ -1,8 +1,11 @@
 /*
  * Copyright (c) 2015-2018, German Aerospace Center (DLR)
- * All rights reserved.
  *
- * This file is part of the Eu:CROPIS flight software.
+ * This file is part of the development version of OUTPOST.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  *
  * Authors:
  * - 2015-2018, Fabian Greif (DLR RY-AVS)
