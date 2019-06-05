c7a219c9df73e4583b8f3f760ad5ebb7b633ae6c
Fabian Greif <Fabian.Greif@dlr.de>
2019-06-05 17:22:59 +0200

    [utils] Add documentation for the Serialize class

--- modules/utils/src/outpost/utils/storage/serialize.h
+++ modules/utils/src/outpost/utils/storage/serialize.h
@@ -38,12 +38,10 @@ struct uint24_t
 };
 
 /**
- * \author Fabian Greif
+ * Serialize data as big-endian into raw byte arrays.
+ *
+ * The supported types can be extended through overloads of the `SerializeBigEndianTraits` class.
  */
-// LCOV_EXCL_START
-// Functions are tested in \c test/unit/test_byteorder.cpp
-// But as they are inline functions lcov can't generate useful
-// coverage reports for them
 class Serialize
 {
 public:
@@ -236,19 +234,15 @@ private:
     uint8_t* mBuffer;
     uint8_t* mBegin;
 };
-// LCOV_EXCL_STOP
-// LCOV_EXCL_END
 
-// ----------------------------------------------------------------------------
 /**
- * Deserialize the application data of a SPP/PUS packet.
+ * Deserialize big-endian data from raw byte arrays.
  *
- * The read<uint8_t>(), read<uint16_t>() and read<uint32_t>() functions read the number of
- * bits from the current location and move the data pointer forward correspondingly. The
- * peek8(), peek16() and peek32() read a value n bytes in front of the current location
- * and *don't* move the data pointer.
+ * The `read<T>()` functions read the number of bits from the current location and move the
+ * data pointer forward correspondingly. The peek<T>() read a value n bytes in front of the
+ * current location and *don't* move the data pointer.
  *
- * \author Fabian Greif
+ * The supported types can be extended through overloads of the `SerializeBigEndianTraits` class.
  */
 class Deserialize
 {
--- modules/utils/src/outpost/utils/storage/serialize_little_endian.h
+++ modules/utils/src/outpost/utils/storage/serialize_little_endian.h
@@ -27,14 +27,10 @@
 namespace outpost
 {
 /**
- * Serialize data in little endian byte order
+ * Serialize data in little endian byte order.
  *
- * \author Fabian Greif
+ * The supported types can be extended through overloads of the `SerializeLittleEndianTraits` class.
  */
-// LCOV_EXCL_START
-// Functions are tested in \c test/unit/test_byteorder.cpp
-// But as they are inline functions lcov can't generate useful
-// coverage reports for them
 class SerializeLittleEndian
 {
 public:
@@ -186,19 +182,15 @@ private:
     uint8_t* mBuffer;
     uint8_t* mBegin;
 };
-// LCOV_EXCL_STOP
-// LCOV_EXCL_END
 
-// ----------------------------------------------------------------------------
 /**
- * Deserialize
+ * Deserialize big-endian data from raw byte arrays.
  *
- * The read<uint8_t>(), read<uint16_t>() and read<uint32_t>() functions read the number of
- * bits from the current location and move the data pointer forward correspondingly. The
- * peek8(), peek16() and peek32() read a value n bytes in front of the current location
- * and *don't* move the data pointer.
+ * The `read<T>()` functions read the number of bits from the current location and move the
+ * data pointer forward correspondingly. The peek<T>() read a value n bytes in front of the
+ * current location and *don't* move the data pointer.
  *
- * \author Fabian Greif
+ * The supported types can be extended through overloads of the `SerializeBigEndianTraits` class.
  */
 class DeserializeLittleEndian
 {
